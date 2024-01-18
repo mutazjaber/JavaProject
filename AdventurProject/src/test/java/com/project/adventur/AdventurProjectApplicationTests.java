a3348fb21d36564ecdcab9b25d75e74109d53842
mutaz jaber <mutaz-mhj@hotmail.com>
2024-01-18 16:34:30 +0200

    first

---
+++ AdventurProject/src/test/java/com/project/adventur/AdventurProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.adventur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
