f4b566194c3786ce887815c4bba504a4b03ef35b
habibGrande <snomo22dy@gmail.com>
2024-01-21 17:01:07 +0200

    login and register and somepages

--- AdventurProject/src/main/java/com/project/adventure/controllers/HomeController.java
+++ AdventurProject/src/main/java/com/project/adventure/controllers/HomeController.java
@@ -5,9 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	
 	@RequestMapping("/")
     public String homePage() {
         return "home.jsp";
     }  
+	@RequestMapping("/login")
+    public String loginPage() {
+        return "login.jsp";
+    }  
+	@RequestMapping("/register")
+    public String registerPage() {
+        return "register.jsp";
+    }  
+	@RequestMapping("/plan")
+    public String planPage() {
+        return "plan.jsp";
+    }  
+	@RequestMapping("/userPlans")
+    public String userPlanPage() {
+        return "userPlans.jsp";
+    }  
+	
+	
+	
 
 }
